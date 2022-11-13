97feef1e7ebc014c0b1c29965b87f444500a0c66
Ananya0511 <dananya028@gmail.com>
2022-11-13 09:58:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+// eslint-disable-next-line
+import {BrowserRouter, BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Error404 from './screens/Error404';
+import Home from './screens/Home';
+import Playground from './screens/Playground';
 
 function App() {
   return (
-    <div className="App">
-    Code Deck
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/playground" element={<Playground />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
